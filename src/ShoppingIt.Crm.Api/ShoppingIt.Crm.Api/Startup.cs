153b32b4622eec691febc8fe4b5c2b4ef189be24
Kevin Stewart <kevin-stewart1@hotmail.co.uk>
2020-12-16 16:55:43 +0000

    Implement Hashing Services
    Added hashing services using KeyDerivation Pbkdf2 with KeyDerivationPrf HMACSHA1

--- src/ShoppingIt.Crm.Api/ShoppingIt.Crm.Api/Startup.cs
+++ src/ShoppingIt.Crm.Api/ShoppingIt.Crm.Api/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ShoppingIt.Crm.Api.Injection;
 
 namespace ShoppingIt.Crm.Api
 {
@@ -26,12 +20,14 @@ namespace ShoppingIt.Crm.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ShoppingIt.Crm.Api", Version = "v1" });
             });
+
+            services.InjectShoppingItServices();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/ShoppingIt.Crm.Api/ShoppingIt.Crm.Tests/HashServiceTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using ShoppingIt.Crm.Core.Services.Hash;
+using System;
+
+namespace ShoppingIt.Crm.Tests
+{
+    [TestFixture]
+    public class HashServiceTest
+    {
+        private IHashService hashService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            hashService = new HashService();
+        }
+
+        [Test]
+        public void Hash_GenerateSalt_ReturnsValidSalt()
+        {
+            var result = hashService.GenerateSalt();
+
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        [TestCase("Password@246")]
+        [TestCase(":{~:!!(*^&)*(^&£$£")]
+        [TestCase("74657;'[[:{~:}{@`¬!")]
+        public void Hash_HashString_ReturnsHashedString(string password)
+        {
+            var salt = hashService.GenerateSalt();
+
+            var hash = hashService.Hash(password, salt);
+
+            Assert.IsNotNull(hash);
+        }
+
+        [Test]
+        [TestCase(":{~:!!(*^&)*(^&£$£", "WLjYRz0KR40ufn4RwbU+Bw==", ExpectedResult = "HiWmrytVcKCjKvZoPzfyO12no/kJA+UoVw42DGDppQ0=")]
+        [TestCase("74657;'[[:{~:}{@`¬!", "K2ay+mz5pZCEqBZUz/OVWg ==", ExpectedResult = "PhnQkdwVq4mczrF2aKRcWZrFDcJgwnlnhTTyDiHOacc=")]
+        public string Hash_HashString_ReturnsHashString(string password, string salt)
+        {
+            return hashService.Hash(password, salt);
+        }
+
+        [Test]
+        [TestCase(":{~:!!(*^&)*(^&£$£", "WLjYRz0KR40ufn4RwbU+Bw==", "HiWmrytVcKCjKvZoPzfyO12no/kJA+UoVw42DGDppQ0=", ExpectedResult = true)]
+        [TestCase("74657;'[[:{~:}{@`¬!", "K2ay+mz5pZCEqBZUz/OVWg ==", "PhnQkdwVq4mczrF2aKRcWZrFDcJgwnlnhTTyDiHOacc=", ExpectedResult = true)]
+        public bool Hash_IsValid_ReturnsTrue(string password, string salt, string passwordHash)
+        {
+            return hashService.IsValid(passwordHash, hashService.Hash(password, salt));
+        }
+
+        [Test]
+        [TestCase(":{~:!!(*^&)*(^&£$£", "WLjYRz0KR40ufn4RwbU+Bw==", "HiWmrytVcKCjKvZO12no/kJA+UoVw42DGDppQ0=", ExpectedResult = false)]
+        [TestCase("74657;'[[:{~:}{@`¬!", "K2ay+mz5pZCEqBZUz/OVWg ==", "PhnQkdwVq4mczrF2cWZrFDcJgwnlnhTTyDiHOacc=", ExpectedResult = false)]
+        public bool Hash_IsNotValid_ReturnsFalse(string password, string salt, string passwordHash)
+        {
+            return hashService.IsValid(passwordHash, hashService.Hash(password, salt));
+        }
+    }
+}
